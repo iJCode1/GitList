dde747fdd39dedb7f9c334ef62b9107f9170e7cc
Joel <joeldome17@gmail.com>
2022-10-01 23:06:41 -0500

    Layout styles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const ProfileStyled = styled.div`
-
+  grid-area: profile;
+  background-color: #efe467;
 `;
 
 function Profile() {
--- src/components/Repo-List.jsx
+++ src/components/Repo-List.jsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 const RepoListStyled = styled.div`
-
+  grid-area: repo-list;
+  background-color: #6767ef;
 `;
 
 function RepoList() {
