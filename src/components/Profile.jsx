1b51565f8520d3b2e449ebaf24e683f149c50921
Joel <joeldome17@gmail.com>
2022-10-01 21:54:47 -0500

    Layout Component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ProfileStyled = styled.div`
+
+`;
+
+function Profile() {
+  return (
+    <ProfileStyled>
+      Profilee
+    </ProfileStyled>
+  )
+}
+
+export default Profile;
